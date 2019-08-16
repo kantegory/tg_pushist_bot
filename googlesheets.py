9214164b1df733c08192f7bcdb8980949e2456e6
David Dobryakov <32088636+kantegory@users.noreply.github.com>
2019-08-17 01:06:25 +0300

    Update googlesheets.py

--- googlesheets.py
+++ googlesheets.py
@@ -7,7 +7,7 @@ from google.auth.transport.requests import Request
 from pprint import pprint
 
 
-def sheets_connect(scopes, spreadsheet_id, ranges):
+def sheets_connect(scopes, spreadsheet_id=None, ranges=None):
     """Shows basic usage of the Sheets API.
     Prints values from a sample spreadsheet.
     """
@@ -44,6 +44,56 @@ def sheets_connect(scopes, spreadsheet_id, ranges):
     return service
 
 
+def create_sheet(scopes, title):
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', scopes)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('sheets', 'v4', credentials=creds)
+
+    spreadsheet_body = {"properties": {"title": title}}
+
+    request = service.spreadsheets().create(body=spreadsheet_body)
+
+    # request = service.spreadsheets()
+    response = request.execute()
+    spreadsheet_id, spreadsheet_url = response["spreadsheetId"], response["spreadsheetUrl"]
+
+    return spreadsheet_id, spreadsheet_url, service
+
+
+def create_new_list(spreadsheet_id, service, sheet_title):
+    sheet = service.spreadsheets()
+    request = sheet.batchUpdate(spreadsheetId=spreadsheet_id, body={
+          "requests": [
+            {
+              "addSheet": {
+                "properties": {
+                    "title": sheet_title,
+                }
+              }
+            }
+          ]
+        }
+    )
+    response = request.execute()
+    pprint(response)
+
+
 def get_sheets_value(service, spreadsheet_id, ranges):
     # simple get all of values in range
     request = service.spreadsheets().values().batchGet(
@@ -56,7 +106,7 @@ def get_sheets_value(service, spreadsheet_id, ranges):
     pprint(response['valueRanges'][0]['values'][0])
 
 
-def set_sheets_value(service, spreadsheet_id, ranges, request_time=None, response_time=None, response_username=None, response_text=None):
+def set_sheets_value(service, spreadsheet_id, ranges, values):
     sheet = service.spreadsheets()
     request = sheet.values().batchUpdate(spreadsheetId=spreadsheet_id, body={
         "valueInputOption": "USER_ENTERED",
@@ -64,8 +114,21 @@ def set_sheets_value(service, spreadsheet_id, ranges, request_time=None, respons
             {"range": ranges,
              "majorDimension": "ROWS",
              # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
-             "values": [[str(request_time), str(response_time), str(response_username), str(response_text)]]}
+             "values": [values]}
         ]
+    })
+    response = request.execute()
+    pprint(response)
+
+
+def append_sheets_value(service, spreadsheet_id, ranges, values):
+    sheet = service.spreadsheets()
+    request = sheet.values().append(spreadsheetId=spreadsheet_id, range=ranges, valueInputOption="USER_ENTERED", body={
+        "valueInputOption": "USER_ENTERED",
+        "range": ranges,
+        "majorDimension": "ROWS",
+        # сначала заполнять ряды, затем столбцы (т.е. самые внутренние списки в values - это ряды)
+        "values": [values]
     })
     response = request.execute()
     pprint(response)
@@ -76,10 +139,23 @@ def main():
     scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
     # The ID and range of a sample spreadsheet.
-    spreadsheet_id = 'YOUR-SPREADSHEET-ID'
-    ranges = 'YOUR RANGE IN SPREADSHEET'
-
-    sheets_connect(scopes, spreadsheet_id, ranges)
+    # spreadsheet_id = '1PMHlClUrQc8fPZRVRPwprthY2jRCWt2ZMh-I32FK9ko'
+    # ranges = 'Лист1!A2:E2'
+    #
+    # service = sheets_connect(scopes)
+    title = "Статистика пушиста"
+    spreadsheet_id, spreadsheet_url, service = create_sheet(scopes, title)
+    sheet_title = 'Статистика по дням'
+    create_new_list(spreadsheet_id, service, sheet_title)
+    curr_row = 1
+    ranges = sheet_title + '!A' + str(curr_row) + ':F' + str(curr_row)
+    curr_row += 1
+    new_ranges = sheet_title + '!A' + str(curr_row) + ':F' + str(curr_row)
+    values = ['День', 'Кол-во пользователей:', 'Кол-во оплат:',	'Кол-во использования промокодов:',	'Кол-во запросов созданных за сутки:', 'Кол-во запросов отправленных за сутки:']
+    set_sheets_value(service, spreadsheet_id, ranges, values)
+    set_sheets_value(service, spreadsheet_id, new_ranges, values)
+    # append_sheets_value(service, spreadsheet_id, new_ranges, values)
+    print(spreadsheet_id, spreadsheet_url)
 
 
 if __name__ == "__main__":
